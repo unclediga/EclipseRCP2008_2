3411df82bff21ff4db70abebe0d8724932b66dc5
BorodinovI <igordin@yandex.ru>
2014-04-14 19:50:49 +0400

    Собственые файлики.Не работает нифига. Буду обновлять из исходников к книге.

---
+++ src/com/qualityeclipse/favorites/handlers/OpenFavoritesViewHandler.java
@@ -0,0 +1,58 @@
+package com.qualityeclipse.favorites.handlers;
+
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.IHandler;
+import org.eclipse.core.commands.IHandlerListener;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+import com.qualityeclipse.favorites.views.FavoritesView;
+
+public class OpenFavoritesViewHandler implements IHandler {
+
+	@Override
+	public void addHandlerListener(IHandlerListener handlerListener) {
+	}
+
+	@Override
+	public void dispose() {
+	}
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		// Get the active window
+		IWorkbenchWindow window = HandlerUtil
+				.getActiveWorkbenchWindowChecked(event);
+		if (window == null)
+			return null;
+		// Get the active page
+		IWorkbenchPage page = window.getActivePage();
+		if (page == null)
+			return null;
+		// Open and activate the Favorites view
+		try {
+			page.showView(FavoritesView.ID);
+		} catch (PartInitException e) {
+			//FavoritesLog.logError("Failed to open the Favorites view", e);
+		}
+		return null;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return false;
+	}
+
+	@Override
+	public boolean isHandled() {
+		return false;
+	}
+
+	@Override
+	public void removeHandlerListener(IHandlerListener handlerListener) {
+	}
+
+}
