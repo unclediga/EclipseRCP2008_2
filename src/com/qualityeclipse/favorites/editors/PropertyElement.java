39bd392563504dd18f9673689ae45c1877c9730d
BorodinovI <igordin@yandex.ru>
2014-10-07 16:48:15 +0400

    Editor model

---
+++ src/com/qualityeclipse/favorites/editors/PropertyElement.java
@@ -0,0 +1,18 @@
+package com.qualityeclipse.favorites.editors;
+
+public abstract class PropertyElement {
+	public static final PropertyElement[] NO_CHILDREN = {};
+	private PropertyElement parent;
+
+	public PropertyElement(PropertyElement parent) {
+		this.parent = parent;
+	}
+
+	public PropertyElement getParent() {
+		return parent;
+	}
+
+	public abstract PropertyElement[] getChildren();
+
+	public abstract void removeFromParent();
+}
